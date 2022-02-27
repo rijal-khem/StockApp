d7adbe1af1b1733829525a4f619cc46817d29276
rizal <rijalkhem187@gmail.com>
2022-02-27 17:02:56 -0500

    Prod Db connection string added. server start script working.

--- serverStart.sh
+++ serverStart.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/zsh
 
-echo "Stopping Current SpringBoot :StockApp EC2 instance."
+echo "At Step : 1"
+echo "Stopping Springboot Application"
+echo "The pid of running app at port :8181"
+lsof -ti :8181
+kill -9 $(lsof -ti :8181)
 
 
-echo "Starting SpringBoot:StockApp instance in EC2 instance"
+echo "Step 2"
+
+rm -rf /home/ubuntu/stockApp/server/deploy.old
+mv  /home/ubuntu/stockApp/server/deploy.new /home/ubuntu/stockApp/server/deploy.old
+cp -r /home/ubuntu/artifacts  /home/ubuntu/stockApp/server/deploy.new
+
+echo "Step : 3"
+echo "Starting new instance of StockApp"
+echo "Check logs at /home/ubuntu/stockApp/log/stockApp.log"
+java -jar -Dspring.profiles.active=prod /home/ubuntu/stockApp/server/deploy.new/StockApp-0.0.1.jar >> /home/ubuntu/stockApp/log/stockApp.log
+echo "The Pid of new running instance is "
