119b1d2a3cd956c02e57819015eee022a3965d2f
rizal <rijalkhem187@gmail.com>
2022-02-27 18:09:47 -0500

    Correction to run app in background and complete the script.

--- serverStart.sh
+++ serverStart.sh
@@ -16,7 +16,7 @@ cp -r /home/ubuntu/artifacts  /home/ubuntu/stockApp/server/deploy.new
 echo "Step : 3"
 echo "Starting new instance of StockApp"
 echo "Check logs at /home/ubuntu/stockApp/log/stockApp.log"
-java -jar -Dspring.profiles.active=prod /home/ubuntu/stockApp/server/deploy.new/StockApp-0.0.1.jar >> /home/ubuntu/stockApp/log/stockApp.log &
+java -jar -Dspring.profiles.active=prod /home/ubuntu/stockApp/server/deploy.new/StockApp-0.0.1.jar &2>>1 /home/ubuntu/stockApp/log/stockApp.log
 echo "The Pid of new running instance is "
-tail -f /home/ubuntu/stockApp/log/stockApp.log
+
 
